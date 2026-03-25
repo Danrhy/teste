0bcf00f9cedb3d9d5eb2d2b2aee6957505214395
Danrhy <danrhy@gmail.com>
2026-03-25 13:27:13 -0300

    alteração de background

--- App.js
+++ App.js
@@ -13,7 +13,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#606060',
+    backgroundColor: '#ce1919',
     alignItems: 'center',
     justifyContent: 'center',
   },
